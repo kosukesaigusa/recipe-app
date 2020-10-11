8d2a73536f5f319b4352fd48fb2c517273f1ddea
lightnning <spde6g59@arrow.ocn.ne.jp>
2020-10-11 18:55:20 +0900

    参考：signin_page

--- lib/presentation/contact/contact_model.dart
+++ lib/presentation/contact/contact_model.dart
@@ -1,5 +1,46 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ContactModel extends ChangeNotifier {
-  Future fetchContact(context) async {}
+  // Future fetchContact(context) async {}
+  String mail = '';
+  String password = '';
+
+  Future login() async {
+    if (mail.isEmpty) {
+      throw ('メールアドレスを入力してください');
+    }
+    if (password.isEmpty) {
+      throw ('パスワードを入力してください');
+    }
+
+    if (password.length < 8 || password.length > 20) {
+      throw ('パスワードは8文字以上20文字以内です');
+    }
+
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: mail,
+        password: password,
+      );
+    } catch (e) {
+      _errorMessage(e.code);
+    }
+  }
+}
+
+String _errorMessage(e) {
+  switch (e) {
+    case 'invalid-email':
+      return 'メールアドレスを正しい形式で入力してください';
+    case 'wrong-password':
+      return 'パスワードが間違っています';
+    case 'user-not-found':
+      return 'ユーザーが見つかりません';
+    case 'user-disabled':
+      return 'ユーザーが無効です';
+    default:
+      return '不明なエラーです';
+  }
+
 }
--- lib/presentation/contact/contact_page.dart
+++ lib/presentation/contact/contact_page.dart
@@ -1,19 +1,113 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:recipe/presentation/contact/contact_model.dart';
+import 'package:recipe/presentation/signup/signup_page.dart';
 
 class ContactPage extends StatelessWidget {
+  final mailController = TextEditingController();
+  final passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<ContactModel>(
-      create: (_) => ContactModel()..fetchContact(context),
+      create: (_) => ContactModel(),
       child: Scaffold(
         appBar: AppBar(
           centerTitle: true,
           title: Text("お問い合わせ"),
         ),
-        body: Text('お問い合わせページ'),
+        body: Consumer<ContactModel>(builder: (context, model, child) {
+          return Container(
+              child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  controller: mailController,
+                  onChanged: (text) {
+                    model.mail = text.trim();
+                  },
+                  maxLines: 1,
+                  decoration: InputDecoration(
+                    labelText: 'メールアドレス',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                //パスワード
+                TextFormField(
+                  controller: passwordController,
+                  onChanged: (text) {
+                    model.password = text;
+                  },
+                  obscureText: true,
+                  maxLines: 1,
+                  decoration: InputDecoration(
+                    labelText: 'パスワード',
+                    // errorText: '８文字以上20文字以内',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: RaisedButton(
+                    child: Text('ログイン'),
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                    onPressed: () async {
+                      try {
+                        await model.login();
+                      } catch (e) {
+                        _showTextDialog(context, e.toString());
+                      }
+                    },
+                  ),
+                ),
+                FlatButton(
+                  child: Text(
+                    '新規登録はこちら',
+                  ),
+                  textColor: Colors.blue,
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SignUpPage(),
+                      ),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ));
+        }),
       ),
     );
   }
+}
+
+_showTextDialog(context, message) async {
+  await showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(message),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('OK'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
 }
