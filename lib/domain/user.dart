743b09fc402cb480240d6737ec046e3320717fd3
kosukesaigusa <saigusa758cloudy@gmail.com>
2020-10-11 18:21:51 +0900

    implement User class.

---
+++ lib/domain/user.dart
@@ -0,0 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class User {
+  User(this.createdAt, this.email, this.userId);
+
+  Timestamp createdAt;
+  String email;
+  String userId;
+}
