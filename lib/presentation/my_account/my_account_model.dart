80e3abe18aa0c75923e8cf9128b86b6fc46547b3
kosukesaigusa <saigusa758cloudy@gmail.com>
2020-10-12 23:44:13 +0900

    #22 Connect sign in and sign out methods to Firebase Auth

--- lib/presentation/my_account/my_account_model.dart
+++ lib/presentation/my_account/my_account_model.dart
@@ -1,6 +1,34 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class MyAccountModel extends ChangeNotifier {
-  // Future fetchMyAccount(context) async {}
+  bool isLoading = false;
+  String mail = '';
 
+  Future fetchMyAccount() async {
+    // ここにユーザーのメールアドレスを取得する処理を書く
+    // ...
+    // ...
+    // this.mail = ユーザーのメールアドレス
+    notifyListeners();
+  }
+
+  Future signOut() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+    } catch (e) {
+      print('${e.code}: $e');
+      throw ('エラーが発生しました');
+    }
+  }
+
+  void startLoading() {
+    this.isLoading = true;
+    notifyListeners();
+  }
+
+  void endLoading() {
+    this.isLoading = false;
+    notifyListeners();
+  }
 }
