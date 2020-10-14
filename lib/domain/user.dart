2e6dafaee6a824d4dabf4d558938f43b2c9c1d54
ryunosuke <kr07031321@gmail.com>
2020-10-15 01:52:39 +0900

    #27current_userEmail_view ドメインのuser修正 Myアカウントページにアドレスを反映させた

--- lib/domain/user.dart
+++ lib/domain/user.dart
@@ -1,8 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class User {
-  User(this.createdAt, this.email, this.userId);
-
+  // User(this.createdAt, this.email, this.userId);
+  User(DocumentSnapshot doc) {
+    userId = doc.id;
+    email = doc.data()['email'];
+    createdAt = doc.data()['createdAt'];
+  }
   Timestamp createdAt;
   String email;
   String userId;
--- lib/presentation/my_account/my_account_model.dart
+++ lib/presentation/my_account/my_account_model.dart
@@ -1,16 +1,34 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class MyAccountModel extends ChangeNotifier {
   bool isLoading = false;
-  String mail = '';
+  String mail;
+  User user;
 
   Future fetchMyAccount() async {
-    // ここにユーザーのメールアドレスを取得する処理を書く
-    // ...
-    // ...
-    // this.mail = ユーザーのメールアドレス
+    this.isLoading = true;
+    final firebaseUser = FirebaseAuth.instance.currentUser;
+    this.mail = firebaseUser.email;
+    this.user = await fetchUser();
+    this.isLoading = false;
     notifyListeners();
+  }
+
+  Future<User> fetchUser() async {
+    final firebaseUser = FirebaseAuth.instance.currentUser;
+    if (firebaseUser == null) {
+      return null;
+    }
+    final doc = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(firebaseUser.uid)
+        .get();
+    if (!doc.exists) {
+      return null;
+    }
+    return user;
   }
 
   Future signOut() async {
