9e19fea07ac8ae721cd91856f1e037819d09ef4b
ryunosuke <kr07031321@gmail.com>
2020-10-15 01:27:24 +0900

    #8￥ブランチ切り替えるのにコミットしないとできないみたいなのでコメントアウト追加

--- lib/domain/user.dart
+++ lib/domain/user.dart
@@ -2,7 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class User {
   User(this.createdAt, this.email, this.userId);
-
+//
   Timestamp createdAt;
   String email;
   String userId;
