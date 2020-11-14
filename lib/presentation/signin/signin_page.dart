3041335308042c199d87bac5bf93a17f43b10f38
kosukesaigusa <saigusa758cloudy@gmail.com>
2020-11-14 17:50:49 +0900

    Created app icon and edited splash page.

--- lib/presentation/signin/signin_page.dart
+++ lib/presentation/signin/signin_page.dart
@@ -17,58 +17,78 @@ class SignInPage extends StatelessWidget {
       child: ChangeNotifierProvider<SignInModel>(
         create: (_) => SignInModel(),
         child: Scaffold(
+          appBar: PreferredSize(
+            preferredSize: Size.fromHeight(1.0),
+            child: AppBar(),
+          ),
           body: Consumer<SignInModel>(
             builder: (context, model, child) {
               return Stack(
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        TextFormField(
-                          controller: mailController,
-                          onChanged: (text) {
-                            model.changeMail(text);
-                          },
-                          maxLines: 1,
-                          decoration: InputDecoration(
-                            errorText:
-                                model.errorMail == '' ? null : model.errorMail,
-                            labelText: 'メールアドレス',
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 16,
-                        ),
-                        TextFormField(
-                          controller: passwordController,
-                          onChanged: (text) {
-                            model.changePassword(text);
-                          },
-                          obscureText: true,
-                          maxLines: 1,
-                          decoration: InputDecoration(
-                            errorText: model.errorPassword == ''
-                                ? null
-                                : model.errorPassword,
-                            labelText: 'パスワード',
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 16,
-                        ),
-                        SizedBox(
-                          width: double.infinity,
-                          height: 50,
-                          child: RaisedButton(
-                            child: Text('ログイン'),
-                            color: Color(0xFFF39800),
-                            textColor: Colors.white,
-                            onPressed:
-                                model.isMailValid && model.isPasswordValid
+                  Center(
+                    child: SingleChildScrollView(
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          children: <Widget>[
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                SizedBox(
+                                  width: 96,
+                                  child:
+                                      Image.asset('lib/assets/icon_1024.png'),
+                                ),
+                                // Text('シンプルなレシピ'),
+                              ],
+                            ),
+                            SizedBox(
+                              height: 16.0,
+                            ),
+                            TextFormField(
+                              controller: mailController,
+                              onChanged: (text) {
+                                model.changeMail(text);
+                              },
+                              maxLines: 1,
+                              decoration: InputDecoration(
+                                errorText: model.errorMail == ''
+                                    ? null
+                                    : model.errorMail,
+                                labelText: 'メールアドレス',
+                                border: OutlineInputBorder(),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 16,
+                            ),
+                            TextFormField(
+                              controller: passwordController,
+                              onChanged: (text) {
+                                model.changePassword(text);
+                              },
+                              obscureText: true,
+                              maxLines: 1,
+                              decoration: InputDecoration(
+                                errorText: model.errorPassword == ''
+                                    ? null
+                                    : model.errorPassword,
+                                labelText: 'パスワード',
+                                border: OutlineInputBorder(),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 16,
+                            ),
+                            SizedBox(
+                              width: double.infinity,
+                              height: 50,
+                              child: RaisedButton(
+                                child: Text('ログイン'),
+                                color: Color(0xFFF39800),
+                                textColor: Colors.white,
+                                onPressed: model.isMailValid &&
+                                        model.isPasswordValid
                                     ? () async {
                                         model.startLoading();
                                         try {
@@ -85,40 +105,42 @@ class SignInPage extends StatelessWidget {
                                         }
                                       }
                                     : null,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 16,
-                        ),
-                        FlatButton(
-                          child: Text(
-                            '新規登録はこちら',
-                          ),
-                          textColor: Color(0xFFF39800),
-                          onPressed: () {
-                            Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => SignUpPage(),
                               ),
-                            );
-                          },
-                        ),
-                        FlatButton(
-                          child: Text(
-                            'パスワードを忘れた場合',
-                          ),
-                          textColor: Colors.grey,
-                          onPressed: () {
-                            Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => ForgetPasswordPage(),
+                            ),
+                            SizedBox(
+                              height: 16,
+                            ),
+                            FlatButton(
+                              child: Text(
+                                '新規登録はこちら',
                               ),
-                            );
-                          },
+                              textColor: Color(0xFFF39800),
+                              onPressed: () {
+                                Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => SignUpPage(),
+                                  ),
+                                );
+                              },
+                            ),
+                            FlatButton(
+                              child: Text(
+                                'パスワードを忘れた場合',
+                              ),
+                              textColor: Colors.grey,
+                              onPressed: () {
+                                Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => ForgetPasswordPage(),
+                                  ),
+                                );
+                              },
+                            ),
+                          ],
                         ),
-                      ],
+                      ),
                     ),
                   ),
                   model.isLoading
--- lib/presentation/signup/signup_page.dart
+++ lib/presentation/signup/signup_page.dart
@@ -18,146 +18,171 @@ class SignUpPage extends StatelessWidget {
       child: ChangeNotifierProvider<SignUpModel>(
           create: (_) => SignUpModel(),
           child: Scaffold(
+            appBar: PreferredSize(
+              preferredSize: Size.fromHeight(1.0),
+              child: AppBar(),
+            ),
             body: Consumer<SignUpModel>(
               builder: (context, model, child) {
-                return Stack(children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        TextFormField(
-                          controller: mailController,
-                          onChanged: (text) {
-                            model.changeMail(text);
-                          },
-                          maxLines: 1,
-                          decoration: InputDecoration(
-                            errorText:
-                                model.errorMail == '' ? null : model.errorMail,
-                            labelText: 'メールアドレス',
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 16,
-                        ),
-                        TextFormField(
-                          controller: passwordController,
-                          onChanged: (text) {
-                            model.changePassword(text);
-                          },
-                          obscureText: true,
-                          maxLines: 1,
-                          decoration: InputDecoration(
-                            errorText: model.errorPassword == ''
-                                ? null
-                                : model.errorPassword,
-                            labelText: 'パスワード',
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 16,
-                        ),
-                        TextFormField(
-                          controller: confirmController,
-                          onChanged: (text) {
-                            model.changeConfirm(text);
-                          },
-                          obscureText: true,
-                          maxLines: 1,
-                          decoration: InputDecoration(
-                            labelText: 'パスワード（確認用）',
-                            errorText: model.errorConfirm == ''
-                                ? null
-                                : model.errorConfirm,
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 16,
-                        ),
-                        SizedBox(
-                          width: double.infinity,
-                          height: 50,
-                          child: RaisedButton(
-                            child: Text('新規登録'),
-                            color: Color(0xFFF39800),
-                            textColor: Colors.white,
-                            onPressed: model.isMailValid &&
-                                    model.isPasswordValid &&
-                                    model.isConfirmValid
-                                ? () async {
-                                    model.startLoading();
-                                    try {
-                                      await model.signUp();
-                                      await Navigator.pushReplacement(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (context) => TopPage(),
-                                        ),
-                                      );
-                                      model.endLoading();
-                                    } catch (e) {
-                                      showTextDialog(context, e);
-                                      model.endLoading();
-                                    }
-                                  }
-                                : null,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 16,
-                        ),
-                        FlatButton(
-                          child: Text(
-                            'ログインはこちら',
-                          ),
-                          textColor: Color(0xFFF39800),
-                          onPressed: () {
-                            Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => SignInPage(),
+                return Stack(
+                  children: [
+                    Center(
+                      child: SingleChildScrollView(
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            // mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  SizedBox(
+                                    width: 96,
+                                    child:
+                                        Image.asset('lib/assets/icon_1024.png'),
+                                  ),
+                                  // Text('シンプルなレシピ'),
+                                ],
                               ),
-                            );
-                          },
-                        ),
-                        FlatButton(
-                          child: Text(
-                            'ゲストとして利用',
-                          ),
-                          textColor: Colors.grey,
-                          onPressed: () async {
-                            model.startLoading();
-                            try {
-                              await model.signInAnonymously();
-                              model.endLoading();
-                              await Navigator.pushReplacement(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => TopPage(),
+                              SizedBox(
+                                height: 16.0,
+                              ),
+                              TextFormField(
+                                controller: mailController,
+                                onChanged: (text) {
+                                  model.changeMail(text);
+                                },
+                                maxLines: 1,
+                                decoration: InputDecoration(
+                                  errorText: model.errorMail == ''
+                                      ? null
+                                      : model.errorMail,
+                                  labelText: 'メールアドレス',
+                                  border: OutlineInputBorder(),
                                 ),
-                              );
-                            } catch (e) {
-                              showTextDialog(context, e.toString());
-                              model.endLoading();
-                            }
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
-                  model.isLoading
-                      ? Container(
-                          color: Colors.black.withOpacity(0.3),
-                          child: Center(
-                            child: CircularProgressIndicator(),
+                              ),
+                              SizedBox(
+                                height: 16,
+                              ),
+                              TextFormField(
+                                controller: passwordController,
+                                onChanged: (text) {
+                                  model.changePassword(text);
+                                },
+                                obscureText: true,
+                                maxLines: 1,
+                                decoration: InputDecoration(
+                                  errorText: model.errorPassword == ''
+                                      ? null
+                                      : model.errorPassword,
+                                  labelText: 'パスワード',
+                                  border: OutlineInputBorder(),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 16,
+                              ),
+                              TextFormField(
+                                controller: confirmController,
+                                onChanged: (text) {
+                                  model.changeConfirm(text);
+                                },
+                                obscureText: true,
+                                maxLines: 1,
+                                decoration: InputDecoration(
+                                  labelText: 'パスワード（確認用）',
+                                  errorText: model.errorConfirm == ''
+                                      ? null
+                                      : model.errorConfirm,
+                                  border: OutlineInputBorder(),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 16,
+                              ),
+                              SizedBox(
+                                width: double.infinity,
+                                height: 50,
+                                child: RaisedButton(
+                                  child: Text('新規登録'),
+                                  color: Color(0xFFF39800),
+                                  textColor: Colors.white,
+                                  onPressed: model.isMailValid &&
+                                          model.isPasswordValid &&
+                                          model.isConfirmValid
+                                      ? () async {
+                                          model.startLoading();
+                                          try {
+                                            await model.signUp();
+                                            await Navigator.pushReplacement(
+                                              context,
+                                              MaterialPageRoute(
+                                                builder: (context) => TopPage(),
+                                              ),
+                                            );
+                                            model.endLoading();
+                                          } catch (e) {
+                                            showTextDialog(context, e);
+                                            model.endLoading();
+                                          }
+                                        }
+                                      : null,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 16,
+                              ),
+                              FlatButton(
+                                child: Text(
+                                  'ログインはこちら',
+                                ),
+                                textColor: Color(0xFFF39800),
+                                onPressed: () {
+                                  Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => SignInPage(),
+                                    ),
+                                  );
+                                },
+                              ),
+                              FlatButton(
+                                child: Text(
+                                  'ゲストとして利用',
+                                ),
+                                textColor: Colors.grey,
+                                onPressed: () async {
+                                  model.startLoading();
+                                  try {
+                                    await model.signInAnonymously();
+                                    model.endLoading();
+                                    await Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => TopPage(),
+                                      ),
+                                    );
+                                  } catch (e) {
+                                    showTextDialog(context, e.toString());
+                                    model.endLoading();
+                                  }
+                                },
+                              ),
+                            ],
                           ),
-                        )
-                      : SizedBox(),
-                ]);
+                        ),
+                      ),
+                    ),
+                    model.isLoading
+                        ? Container(
+                            color: Colors.black.withOpacity(0.3),
+                            child: Center(
+                              child: CircularProgressIndicator(),
+                            ),
+                          )
+                        : SizedBox(),
+                  ],
+                );
               },
             ),
           )),
