42dce26ca1d08166b02bae536b4517be29ecdc17
kosukesaigusa <saigusa758cloudy@gmail.com>
2020-11-16 16:15:11 +0900

    feat(domain/recipe_edit_page.dart): newly added a recipe edit class

---
+++ lib/domain/recipe_edit.dart
@@ -0,0 +1,43 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class RecipeEdit {
+  RecipeEdit() {
+    this.updatedAt = null;
+    this.name = '';
+    this.thumbnailURL = '';
+    this.thumbnailName = '';
+    this.imageURL = '';
+    this.imageName = '';
+    this.content = '';
+    this.reference = '';
+    this.tokenMap = {};
+    this.isEdited = false;
+    this.willPublish = false;
+    this.agreeGuideline = false;
+    this.errorName = '';
+    this.errorContent = '';
+    this.errorReference = '';
+    this.isNameValid = true;
+    this.isContentValid = true;
+    this.isReferenceValid = true;
+  }
+
+  Timestamp updatedAt;
+  String name;
+  String thumbnailName;
+  String thumbnailURL;
+  String imageURL;
+  String imageName;
+  String content;
+  String reference;
+  Map<String, bool> tokenMap;
+  bool isEdited;
+  bool willPublish;
+  bool agreeGuideline;
+  String errorName;
+  String errorContent;
+  String errorReference;
+  bool isNameValid;
+  bool isContentValid;
+  bool isReferenceValid;
+}
